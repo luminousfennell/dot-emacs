f5c43d7db29f52349daf0e54229a5881b4b97c44
Luminous Fennell <mstrlu@gmx.net>
2013-07-31 21:57:38 +0200

    Manage agenda files in a list

--- init-org.el
+++ init-org.el
@@ -1,11 +1,17 @@
 (require 'org-install)
 
+;; agenda files... set in the load hook in order to access org-directory
+(add-hook 'org-load-hook
+	  (lambda ()
+	    (setq org-agenda-files
+		  (concat (file-name-as-directory org-directory) "agenda-files.lst"))))
+
 ;; global keybindings
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cc" 'org-capture)
 
 ;; TODO: find a solution to sanely use spell checking in org mode
-(add-hook 'org-mode-hook (lambda () (flyspell-mode 0)))
+(add-hook 'org-mode-hook (lambda () (flyspell-mode -1)))
 
 
 ;; Automatically close a todo item when all children are finished
