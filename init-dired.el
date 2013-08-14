a89e394e035db4c010d86ac7333d56c7bc2c5e7e
Luminous Fennell <mstrlu@gmx.net>
2013-08-14 20:30:42 +0200

    Setup dired-async (with some workarounds)

--- init-dired.el
+++ init-dired.el
@@ -10,15 +10,30 @@
  dired-omit-files "^\\.?#\\|^\\.$\\|^\\.[^\\.].*$"
  dired-omit-verbose nil
  dired-recursive-copies 'always
- dired-recursive-deletes 'always
- )
+ dired-recursive-deletes 'always)
 
 ;; hook
 (add-hook 'dired-mode-hook
 	  (lambda ()
 	    (auto-revert-mode t)
 	    ;; keybindings
-	    (evil-define-key 'normal dired-mode-map ")" 'dired-details-toggle)))
+	    (evil-define-key 'normal dired-mode-map ")"
+	      'dired-details-toggle)
+	    ;; Override dired-async's broken delete (it segfaults)
+	    (require 'dired-async)
+	    (evil-define-key
+	      'normal
+	      dired-mode-map "D" 'my-dired-async-delete)))
+
+(defun my-dired-async-delete ()
+  (interactive)
+  (when (y-or-n-p (format "Really delete these %d files? "
+			  (length (dired-get-marked-files))))
+    (dired-map-over-marks
+     (progn
+       (let ((f (dired-get-filename)))
+	 (dired-delete-file f dired-recursive-deletes))) nil)))
+
 
 
 ;;dired-x for hidden view
