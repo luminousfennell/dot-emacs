0e0af37e505bcb330847f9bd9d13b1998087ead2
Luminous Fennell <mstrlu@gmx.net>
2014-03-23 19:06:21 +0100

    migrate org init

--- init.el
+++ init.el
@@ -1,6 +1,5 @@
 (load-file "~/.emacs.d/init-common.el")
 
-(load-file "~/.emacs.d/init-org.el")
 (load-file "~/.emacs.d/init-dired.el")
 (load-file "~/.emacs.d/init-ispell.el")
 (load-file "~/.emacs.d/init-coq.el")
---
+++ lib/my-org-utils.el
@@ -0,0 +1,8 @@
+(provide 'my-org-utils)
+
+;; Automatically close a todo item when all children are finished
+;; snippet taken from the org manual
+(defun org-summary-todo (n-done n-not-done)
+  "Switch entry to DONE when all subentries are done, to TODO otherwise."
+  (let (org-log-done org-log-states)   ; turn off logging
+    (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
