05d676a7d2c8dcde88a46c70d9d4cf9d7351b206
Luminous Fennell <fennell@informatik.uni-freiburg.de>
2014-05-13 18:47:22 +0200

    my-synctex: Add forward sync

--- lib/my-synctex.el
+++ lib/my-synctex.el
@@ -1,8 +1,16 @@
 (defun my-synctex-goto (filename line col)
   "Move select buffer named (file-name-nondirectory FILENAME) and move point to LINE and COLUMN."
-  (let ((buffer-name (file-name-nondirectory filename)))
-    (goto-line line buffer-name)
-    (when (and (<= 0 col) (<= col (line-end-position)))
-      (right-char col))))
+  (find-file filename)
+  (goto-line line)
+  (when (and (<= 0 col) (<= col (line-end-position)))
+    (right-char col)))
+
+(defun my-synctex-zathura ()
+  (interactive)
+  (let ((line (line-number-at-pos))
+	(col (- (point) (line-beginning-position)))
+	(pdf-name (concat (file-name-sans-extension (buffer-file-name)) ".pdf"))
+	(tex-name (buffer-file-name)))
+    (call-process "zathura" nil 0 nil "--synctex-forward" (format "%d:%d:%s" line col tex-name) pdf-name)))
 
 (provide 'my-synctex)
